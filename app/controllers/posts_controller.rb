027993c768b1b5f8907867f64f7226ee3993ff7e
jonathon <ahnjon77@gmail.com>
2021-10-13 23:02:16 -0700

    completes scaffolding guide

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,19 +4,36 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
   end
 
   # GET /posts/1 or /posts/1.json
   def show
+    @post = Post.find(params[:id])
+ 
+    respond_to do |format|
+      format.html  # show.html.erb
+      format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/new
   def new
     @post = Post.new
+
+    respond_to do |format|
+      format.html  # new.html.erb
+      format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/1/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
   # POST /posts or /posts.json
